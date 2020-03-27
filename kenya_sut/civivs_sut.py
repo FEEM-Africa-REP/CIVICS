81ffa24ef8a238d4e27288d975c955442b9ffa9c
nigolred <nicolo.golinucci@gmail.com>
2020-03-27 13:46:24 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/FEEM-Africa-REP/CIVICS_Kenya""
    This reverts commit 57e91a459d6678fdb114bdeb302e3cbc48a41704.

--- kenya_sut/civivs_sut.py
+++ kenya_sut/civivs_sut.py
@@ -42,6 +42,11 @@ class C_SUT:
         # computing total production vector (X)
         self.X = pd.DataFrame(self.Y.sum(axis=1) + self.Z.sum(axis=1), index=self.Z.index, columns=['Total Production'])
         
+        # computing matrices of coefficients
+        self.z = pd.DataFrame(self.Z.values @ np.linalg.inv(self.X.values  * np.identity(len(self.X))), index=self.Z.index, columns=self.Z.columns)
+        self.va = pd.DataFrame(self.VA.values @ np.linalg.inv(self.X.values  * np.identity(len(self.X))), index=self.VA.index, columns=self.VA.columns)
+        self.s = pd.DataFrame(self.S.values @ np.linalg.inv(self.X.values  * np.identity(len(self.X))), index=self.S.index, columns=self.S.columns) 
+        
 # Probably I would delete this function...       
     def parse(self):
         
@@ -118,9 +123,18 @@ class C_SUT:
                 plt.ylabel('M Ksh')
                 plt.legend(loc=1,bbox_to_anchor = (1.5,1))
                 plt.show()    
-        
+ 
+
+      
+# This function will calculate all the flows with new names in a way after
+# implementing a shock, the user can have access to all the information before 
+# and after shock impelementation.
+
+
+
+    def calc_all(self):
+
                 
-    def calc_all(self):        
         import pandas as pd
         import numpy as np
         
@@ -128,117 +142,66 @@ class C_SUT:
         self.VA_ind = self.VA.index
         self.S_ind = self.S.index
 
-        # computing matrices of coefficients
-        self.z = pd.DataFrame(self.Z.values @ np.linalg.inv(self.X.values  * np.identity(len(self.X))), index=self.Z.index, columns=self.Z.columns)
-        self.va = pd.DataFrame(self.VA.values @ np.linalg.inv(self.X.values  * np.identity(len(self.X))), index=self.VA.index, columns=self.VA.columns)
-        self.s = pd.DataFrame(self.S.values @ np.linalg.inv(self.X.values  * np.identity(len(self.X))), index=self.S.index, columns=self.S.columns)
+        try:
+            # computing The New X_c
+            self.X_c = pd.DataFrame(self.l @ self.Y_c.values , index = self.X.index , columns = self.X.columns)
+            # re-computing flow matrices
+            self.VA_c = pd.DataFrame(self.va.values @ (self.X_c.values  * np.identity(len(self.X_c))),index = self.VA_ind,columns =  self.Z.columns)
+            self.IMP_c = pd.DataFrame(self.imp.values @ (self.X_c.values  * np.identity(len(self.X_c))),index = self.IMP_ind,columns =  self.Z.columns)
+            self.E_c = pd.DataFrame(self.e.values @ (self.X_c.values * np.identity(len(self.X_c))),index = self.E_ind , columns = self.Z.columns)
+            self.Z_c = pd.DataFrame(self.z.values @  (self.X_c.values * np.identity(len(self.X_c))),index = self.Z.index , columns = self.Z.columns)
+            
+        except:
+            raise ValueError('A shock should be Implemented to calculate the new matrices!')
+        
+        
 
-        # re-computing flow matrices
-        self.VA_c = pd.DataFrame(self.va.values @ (self.X.values  * np.identity(len(self.X))),index = self.VA_ind,columns =  self.Z.columns)
-        self.IMP_c = pd.DataFrame(self.imp.values @ (self.X.values  * np.identity(len(self.X))),index = self.IMP_ind,columns =  self.Z.columns)
-        self.E_c = pd.DataFrame(self.e.values @ (self.X.values * np.identity(len(self.X))),index = self.E_ind , columns = self.Z.columns)
-        
-        
-# NG: I think we can more easly build a new function (aggregate) that ask for the level and simply use groupby for every objects returning aggregated version of objects
-# like this:
         
     def aggregate(self, level=4):
         
-        self.X = self.X.groupby(level=level).sum() #and so on
-        # # Aggregating Results
-        # # Aggregation of x Matrix
-        # com_pr = self.x.loc['Commodities']
-        # com_pr.index = com_pr.index.get_level_values(3)
-        # com_pr = com_pr.groupby(axis=0,level=0).sum()
-        
-        # ind_pr = self.x.loc['Activities']
-        # ind_pr.index = ind_pr.index.get_level_values(3)
-        # ind_pr = ind_pr.groupby(axis=0,level=0).sum()
-        
-        # cmdt = []
-        # for i in range(len(com_pr)):
-        #     cmdt.append('Commodities')
-        
-        # inds = []
-        # for i in range(len(ind_pr)):
-        #     inds.append('Activities')    
+        # To consider the case that the user did not calculate the new matrices
+        try:
+            self.X_agg   = self.X.groupby(level=level,sort = False).sum()
+            self.X_c_agg = self.X_c.groupby(level=level,sort = False).sum()
             
-        # new_x = pd.concat([com_pr,ind_pr],axis = 0)
-        # new_x_index = [cmdt+inds,com_pr.index.to_list() + ind_pr.index.to_list()]
-        # new_x.index = new_x_index
-        
-        # self.x_agg = new_x
-
-
-        # # Aggregation of VA Matrix
-        # com_va = self.VA['Commodities']
-        # com_va.columns = com_va.T.index.get_level_values(3)
-        # com_va = com_va.groupby(axis=1,level=0).sum()
-        
-        # ind_va = self.VA['Activities']
-        # ind_va.columns = ind_va.T.index.get_level_values(3)
-        # ind_va = ind_va.groupby(axis=1,level=0).sum()
+            self.VA_agg  = self.VA.groupby(level=level,sort = False).sum()
+            self.VA_c_agg= self.VA_c.groupby(level=level,sort = False).sum()
+            
+            self.Z_agg  = self.Z.groupby(level=level,sort = False).sum()
+            self.Z_c_agg= self.Z_c.groupby(level=level,sort = False).sum()
+            
+        except:
+            
+            self.X_agg   = self.X.groupby(level=level,sort = False).sum()           
+            self.VA_agg  = self.VA.groupby(level=level,sort = False).sum()           
+            self.Z_agg  = self.Z.groupby(level=level,sort = False).sum()
+            
         
 
-        # new_va = pd.concat([com_va,ind_va],axis = 1)
-        # new_va.columns = new_x_index
-        
-        # self.VA_agg = new_va
-    
-
-        # # Aggregation of IMP Matrix
-        # com_im = self.IMP['Commodities']
-        # com_im.columns = com_im.T.index.get_level_values(3)
-        # com_im = com_im.groupby(axis=1,level=0).sum()
-        
-        # ind_im = self.IMP['Activities']
-        # ind_im.columns = ind_im.T.index.get_level_values(3)
-        # ind_im = ind_im.groupby(axis=1,level=0).sum()
-        
-
-        # new_im = pd.concat([com_im,ind_im],axis = 1)
-        # new_im.columns = new_x_index
-        
-        # self.IMP_agg = new_im      
-      
 
     def shock(self, path , sensitivity = False,Y= False , E = False , A= False , VA = False):
         import pandas as pd
+        # In order to keep the original Y, a copy of that will be used for the case
+        # that a shock is going to be implemented
+        
+        
+        self.Y_c = self.Y.copy()
         
         if Y:
             Y_m = pd.read_excel(path, sheet_name = 'Y', index_col = [0] , header = [0])
-            
+
             header = Y_m.columns.to_list()
-            for i in range(len(Y_m)):   
+            index  = Y_m.index.to_list()
+            print(Y_m)
+            
+            for i in range(len(Y_m)): 
                 
-                self.Y.loc[['Commodities',Y_m.loc[str(i+1),header[0]]]] = \
-                           self.Y.loc[['Commodities',Y_m.loc[str(i+1),header[0]]]] + Y_m.loc[str(i+1),header[1]].values
+              
+                            
+                self.Y_c.loc[['Commodities',Y_m.loc[i+1,header[0]]],'Total final demand'] = \
+                            self.Y_c.loc[['Commodities',Y_m.loc[i+1,header[0]]],'Total final demand'] + Y_m.loc[i+1,header[1]]
             
-        if A:
-            A_m = pd.read_excel(path, sheet_name = 'A', index_col = [0] , header = [0])
-            
-            header = A_m.columns.to_list()
-            
-            for i in range(len(A_m)):  
-                
-                if A_m.loc[str(i+1),header[4]] == 'Percentage':
-                    
-                    if A_m.loc[str(i+1),header[5]].values < 1.0 :
-                        coeff = A_m.loc[str(i+1),header[5]].values
-                    else:
-                        coeff = A_m.loc[str(i+1),header[5]].values / 100.0
-                
-                    self.A.loc[[A_m.loc[str(i+1),header[0]]],A_m.loc[str(i+1),header[1]],[A_m.loc[str(i+1),header[2]]],A_m.loc[str(i+1),header[3]]] = \
-                        self.A.loc[[A_m.loc[str(i+1),header[0]]],A_m.loc[str(i+1),header[1]],[A_m.loc[str(i+1),header[2]]],A_m.loc[str(i+1),header[3]]] \
-                            * (1 + coeff )      
-                    
-                
-        # if VA:
-        #     VA_m = pd.read_excel(path, sheet_name = 'VA', index_col = [0] , header = [0])
-        #     header = VA_m.columns.to_list()
-            
-        #     for i in range(len(VA_m)):  
-            
+
             
         
         
