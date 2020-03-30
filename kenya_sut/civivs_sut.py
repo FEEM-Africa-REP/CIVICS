0abae8b7ae957956c31ca438d47e0496c4951b90
Mohammad Amin Tahavori <50955527+mohammadamint@users.noreply.github.com>
2020-03-30 12:48:00 +0200

    Save_all function finished

--- kenya_sut/civivs_sut.py
+++ kenya_sut/civivs_sut.py
@@ -106,6 +106,7 @@ class C_SUT:
             self.X_c_agg = self.X_c.groupby(level=level_x , sort = sort).sum()
             
             self.Y_agg = self.Y.groupby(level=level_x, sort=sort).sum()
+            self.Y_c_agg = self.Y_c.groupby(level=level_x, sort=sort).sum()
             
             self.Z_agg = self.Z.groupby(level = level_x,sort=sort).sum().groupby(axis = 1 , level = level_x , sort = sort).sum()
             self.Z_c_agg = self.Z_c.groupby(level = level_x,sort=sort).sum().groupby(axis = 1 , level = level_x , sort = sort).sum()
@@ -224,39 +225,158 @@ class C_SUT:
                     
 ##################################################################################################################              
 
-    def Save_all(self, path,level = None):
+    def Save_all(self, path,level = None,drop='unused'):
         
         import pandas as pd
+        import xlsxwriter
         
+        if drop != None:
+        
+            S   = self.S.drop(drop,level = 3)
+            S_c = self.S_c.drop(drop,level = 3)
+            s   = self.s.drop(drop,level = 3)
+            s_c = self.s_c.drop(drop,level = 3)
+            S_agg   = self.S_agg.drop(drop)
+            S_c_agg = self.S_c_agg.drop(drop) 
+            
+            VA   = self.VA.drop(drop,level = 3)
+            VA_c = self.VA_c.drop(drop,level = 3)
+            va   = self.va.drop(drop,level = 3)
+            va_c = self.va_c.drop(drop,level = 3)
+            VA_agg   = self.VA_agg.drop(drop)
+            VA_c_agg = self.VA_c_agg.drop(drop)    
+            
+        elif drop == None:
+
+            S   = self.S
+            S_c = self.S_c
+            s   = self.s
+            s_c = self.s_c
+            S_agg   = self.S_agg
+            S_c_agg = self.S_c_agg
+            
+            VA   = self.VA
+            VA_c = self.VA_c
+            va   = self.va
+            va_c = self.va_c
+            VA_agg   = self.VA_agg
+            VA_c_agg = self.VA_c_agg
+            
+
+            
         if level == None:
+            
+            try:
         
-            with pd.ExcelWriter(path + 'Shock_agg_flows.xlsx') as writer:
+                with pd.ExcelWriter(path + '\ Shock_agg_flows.xlsx') as writer:
+                    
+                    self.Z_c_agg.to_excel(writer,sheet_name='Z')
+                    self.X_c_agg.to_excel(writer,sheet_name='X')
+                    VA_c_agg.to_excel(writer,sheet_name='VA')
+                    S_c_agg.to_excel(writer,sheet_name='S')
+                    self.Y_c_agg.to_excel(writer,sheet_name='Y')
+                    
+                with pd.ExcelWriter(path + '\ Shock_coeff.xlsx') as writer:
+                    
+                    self.z_c.to_excel(writer,sheet_name='z')
+                    va_c.to_excel(writer,sheet_name='va')
+                    s_c.to_excel(writer,sheet_name='s')
+        
+                with pd.ExcelWriter(path + '\ Baseline_agg_flows.xlsx') as writer:
+                    
+                    self.Z_agg.to_excel(writer,sheet_name='Z')
+                    self.X_agg.to_excel(writer,sheet_name='X')
+                    VA_agg.to_excel(writer,sheet_name='VA')
+                    S_agg.to_excel(writer,sheet_name='S')
+                    self.Y_agg.to_excel(writer,sheet_name='Y')
+                    
+                with pd.ExcelWriter(path + '\ Baseline_coeff.xlsx') as writer:
+                    
+                    self.z.to_excel(writer,sheet_name='z')
+                    va.to_excel(writer,sheet_name='va')
+                    s.to_excel(writer,sheet_name='s')
+    
+                with pd.ExcelWriter(path + '\ Baseline_flows.xlsx') as writer:
+                    
+                    self.Z.to_excel(writer,sheet_name='Z')
+                    self.X.to_excel(writer,sheet_name='X')
+                    VA.to_excel(writer,sheet_name='VA')
+                    S.to_excel(writer,sheet_name='S')
+                    self.Y.to_excel(writer,sheet_name='Y')
+                    
+                with pd.ExcelWriter(path + '\ Shock_flows.xlsx') as writer:
+                    
+                    self.Z_c.to_excel(writer,sheet_name='Z')
+                    self.X_c.to_excel(writer,sheet_name='X')
+                    VA_c.to_excel(writer,sheet_name='VA')
+                    S_c.to_excel(writer,sheet_name='S')
+                    self.Y_c.to_excel(writer,sheet_name='Y') 
+                    
+                    
+                with pd.ExcelWriter(path + '\ Change_flows.xlsx') as writer:
+
+                    (self.X_c-self.X).to_excel(writer,sheet_name='X')
+                    (VA_c-VA).to_excel(writer,sheet_name='VA')
+                    (S_c-S).to_excel(writer,sheet_name='S')
+                    (self.X_c_agg-self.X_agg).to_excel(writer,sheet_name='X_agg')
+                    (VA_c_agg-VA_agg).to_excel(writer,sheet_name='VA_agg')
+                    (S_c_agg-S_agg).to_excel(writer,sheet_name='S_agg')                                                                         
+            except:
+                raise ValueError('No Shock Implemented so, the level could be only {}'.format('baseline'))
                 
-                self.Z_c_agg.to_excel(writer,sheet_name='Z')
-                self.X_c_agg.to_excel(writer,sheet_name='X')
-                self.VA_c_agg.to_excel(writer,sheet_name='VA')
-                self.S_c_agg.to_excel(writer,sheet_name='S')
-                self.Y_c_agg.to_excel(writer,sheet_name='Y'
                 
-            with pd.ExcelWriter(path + 'Shock_coeff.xlsx') as writer:
-                
-                self.z_c.to_excel(writer,sheet_name='z')
-                self.va_c.to_excel(writer,sheet_name='va')
-                self.s_c.to_excel(writer,sheet_name='s')
+        elif level=='baseline':
+
     
             with pd.ExcelWriter(path + 'Baseline_agg_flows.xlsx') as writer:
                 
                 self.Z_agg.to_excel(writer,sheet_name='Z')
                 self.X_agg.to_excel(writer,sheet_name='X')
-                self.VA_agg.to_excel(writer,sheet_name='VA')
-                self.S_agg.to_excel(writer,sheet_name='S')
-                self.Y_agg.to_excel(writer,sheet_name='Y'
+                VA_agg.to_excel(writer,sheet_name='VA')
+                S_agg.to_excel(writer,sheet_name='S')
+                self.Y_agg.to_excel(writer,sheet_name='Y')
                 
             with pd.ExcelWriter(path + 'Baseline_coeff.xlsx') as writer:
                 
                 self.z.to_excel(writer,sheet_name='z')
-                self.va.to_excel(writer,sheet_name='va')
-                self.s_c_agg.to_excel(writer,sheet_name='s')
+                va.to_excel(writer,sheet_name='va')
+                s.to_excel(writer,sheet_name='s')
+
+            with pd.ExcelWriter(path + 'Baseline_flows.xlsx') as writer:
+                
+                self.Z.to_excel(writer,sheet_name='Z')
+                self.X.to_excel(writer,sheet_name='X')
+                VA.to_excel(writer,sheet_name='VA')
+                S.to_excel(writer,sheet_name='S')
+                self.Y.to_excel(writer,sheet_name='Y')
+                
+              
+        elif level=='shock':
+            
+            with pd.ExcelWriter(path + 'Shock_agg_flows.xlsx') as writer:
+                
+                self.Z_c_agg.to_excel(writer,sheet_name='Z')
+                self.X_c_agg.to_excel(writer,sheet_name='X')
+                VA_c_agg.to_excel(writer,sheet_name='VA')
+                S_c_agg.to_excel(writer,sheet_name='S')
+                self.Y_c_agg.to_excel(writer,sheet_name='Y')
+                
+            with pd.ExcelWriter(path + 'Shock_coeff.xlsx') as writer:
+                
+                self.z_c.to_excel(writer,sheet_name='z')
+                va_c.to_excel(writer,sheet_name='va')
+                s_c.to_excel(writer,sheet_name='s')
+
+                
+            with pd.ExcelWriter(path + 'Shock_flows.xlsx') as writer:
+                
+                self.Z_c.to_excel(writer,sheet_name='Z')
+                self.X_c.to_excel(writer,sheet_name='X')
+                VA_c.to_excel(writer,sheet_name='VA')
+                S_c.to_excel(writer,sheet_name='S')
+                self.Y_c.to_excel(writer,sheet_name='Y') 
+
+                          
 
 ##################################################################################################################
 
