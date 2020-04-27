359185e13d6a417f438b7d843b1baeff8595b60b
Mohammad Amin Tahavori <50955527+mohammadamint@users.noreply.github.com>
2020-04-27 16:32:17 +0200

    exm

--- kenya_sut/shading.py
+++ kenya_sut/shading.py
@@ -7,25 +7,35 @@ Created on Mon Apr 13 17:53:57 2020
 
 import civivs_sut as cvx
 kenya = cvx.C_SUT(r'Database\Kenya_2014_SAM.xlsx')
-kenya.shock(path = r'Database\Shock_shading.xlsx' , Y = True )
+env0=kenya.S
+kenya.shock(path = r'Database\Shock_pulp.xlsx' , Y = True )
 kenya.calc_all()
 kenya.add_dict()
 kenya.aggregate()
 kenya.plot_dv()
 kenya.plot_dx()
 kenya.plot_dp()
+kenya.plot_dS()
+env1=kenya.S_c
 Investment=kenya.Y_c.sum().sum()-kenya.Y.sum().sum()
 #%%
-kenya.shock(path = r'Database\Shock_shading.xlsx' , Z=True ,VA = True)
+kenya.shock(path = r'Database\Shock_pulp.xlsx' , Z=True ,VA = True, S=True)
 kenya.calc_all()
 kenya.add_dict()
 kenya.aggregate()
 kenya.plot_dv()
 kenya.plot_dx()
 kenya.plot_dp()
+kenya.plot_dS()
+env2=kenya.S_c
 Savings=kenya.VA_c.sum().sum()-kenya.VA.sum().sum()
 kenya.optimize(scenario=2)
 #%%
 X_opt = kenya.X_opt
 Y_opt = kenya.Y_opt
+#%%
+results= kenya.results
+#%%
+env_del1 = env2 - env0
+change = env_del1 / env0 * 100
 
