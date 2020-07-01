1dd0d49fe5543dc360eec0d5f7fac19ec69574f8
nigolred <nicolo.golinucci@gmail.com>
2020-07-01 15:48:52 +0200

    Shading_nets

--- CVX_Kenya/Shading_nets.py
+++ CVX_Kenya/Shading_nets.py
@@ -5,22 +5,26 @@ Created on Mon Apr 13 17:53:57 2020
 @author: Amin
 """
 
-import civivs_sut as cvx
+import REP_CVX as cvx
 kenya = cvx.C_SUT(r'Database\Kenya_2014_SAM.xlsx')
 env0=kenya.S
 kenya.shock(path = r'Interventions\Shading_nets.xlsx' , Y = True )
+
 kenya.calc_all()
 kenya.aggregate()
 kenya.add_dict()
+
 kenya.plot_dv()
 kenya.plot_dx()
 kenya.plot_dp()
+
 kenya.plot_dS(indicator='CO2')
 #%%
-kenya.shock(path = r'Database\Shock_shading_update.xlsx' , Z=True ,VA = True, S=True)
+kenya.shock(path = r'Interventions\Shading_nets.xlsx' , Z=True ,VA = True, S=True)
 kenya.calc_all()
 kenya.aggregate()
 kenya.add_dict()
+
 kenya.plot_dv()
 kenya.plot_dx()
 kenya.plot_dp()
@@ -29,3 +33,4 @@ kenya.plot_dS()
 results= kenya.results
 #%%
 kenya.Int_Ass()
+print(kenya.ROI)
