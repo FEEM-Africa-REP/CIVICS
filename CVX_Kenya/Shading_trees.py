1555fccdbab9ec954cb9936284813b306526ec0e
Mohammad Amin Tahavori <50955527+mohammadamint@users.noreply.github.com>
2020-07-30 01:47:05 +0200

    shading_sen1

--- CVX_Kenya/Shading_trees.py
+++ CVX_Kenya/Shading_trees.py
@@ -7,14 +7,14 @@ Created on Mon Apr 13 17:53:57 2020
 
 import REP_CVX as cvx
 kenya = cvx.C_SUT(r'Database\Kenya_2014_SAM.xlsx')
-env0=kenya.S
-kenya.shock(path = r'Interventions\Shading_trees.xlsx' , Y = True )
+sh_path = r'Interventions\New folder\num_hect\2500.xlsx'
+kenya.shock(path = sh_path, Y = True )
 
 kenya.calc_all()
 kenya.aggregate()
 kenya.add_dict()
 
-kenya.sensitivity(parameter='Y')
+#kenya.sensitivity(parameter='Y')
 
 # kenya.plot_dv()
 # kenya.plot_dx()
@@ -23,10 +23,10 @@ kenya.sensitivity(parameter='Y')
 #kenya.plot_dS(indicator='CO2')
 
 #%%
-kenya.plot_dv(unit='M KSH', main_title='Change in the use of commodities', level='Commodities', percent=False, drop=['unused','Capital - Land','Capital - Livestock','Capital - Agriculture','Capital - Machines','Labor - Skilled', 'Labor - Semi Skilled', 'Labor - Unskilled'], color='ocean')
-kenya.plot_dv(unit='M KSH', main_title='Change in the output of activities', level='Activities', percent=False, drop=['unused', 'Taxes', 'Import','Margins'], color='Accent')
+#kenya.plot_dv(unit='M KSH', main_title='Change in the use of commodities', level='Commodities', percent=False, drop=['unused','Capital - Land','Capital - Livestock','Capital - Agriculture','Capital - Machines','Labor - Skilled', 'Labor - Semi Skilled', 'Labor - Unskilled'], color='ocean')
+#kenya.plot_dv(unit='M KSH', main_title='Change in the output of activities', level='Activities', percent=False, drop=['unused', 'Taxes', 'Import','Margins'], color='Accent')
 #%%
-kenya.shock(path = r'Interventions\Shading_trees.xlsx' , Z=True ,VA = True, S=True)
+kenya.shock(path = sh_path , Z=True ,VA = True, S=True)
 
 kenya.calc_all()
 kenya.aggregate()
@@ -37,13 +37,13 @@ kenya.add_dict()
 # kenya.plot_dp()
 #kenya.plot_dS(Type='absolute')
 #%%
-kenya.plot_dv(unit='M KSH', main_title='Change in the use of commodities', level='Commodities', percent=False, drop=['unused','Capital - Land','Capital - Livestock','Capital - Agriculture','Capital - Machines','Labor - Skilled', 'Labor - Semi Skilled', 'Labor - Unskilled'], color='ocean')
-kenya.plot_dv(unit='M KSH', main_title='Change in the output of activities', level='Activities', percent=False, drop=['unused', 'Taxes', 'Import','Margins'], color='Accent')
+#kenya.plot_dv(unit='M KSH', main_title='Change in the use of commodities', level='Commodities', percent=False, drop=['unused','Capital - Land','Capital - Livestock','Capital - Agriculture','Capital - Machines','Labor - Skilled', 'Labor - Semi Skilled', 'Labor - Unskilled'], color='ocean')
+#kenya.plot_dv(unit='M KSH', main_title='Change in the output of activities', level='Activities', percent=False, drop=['unused', 'Taxes', 'Import','Margins'], color='Accent')
 
 #%%
-results= kenya.results
+#results= kenya.results
 #%%
-kenya.Int_Ass(inv_sen=['sensitivity',1],sce_name='shading_C_planting')
+kenya.Int_Ass(sce_name='2500')
 #print(kenya.ROI)
 #%%
-am = kenya.X.index.get_level_values(0,1)
+#am = kenya.X.index.get_level_values(0,1)
