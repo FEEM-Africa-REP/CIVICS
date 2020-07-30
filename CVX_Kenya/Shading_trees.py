65ff600ea07c314cee43258f9da6e4e0993a5a4b
Mohammad Amin Tahavori <50955527+mohammadamint@users.noreply.github.com>
2020-07-30 09:05:30 +0200

    new_shocks

--- CVX_Kenya/Shading_trees.py
+++ CVX_Kenya/Shading_trees.py
@@ -7,7 +7,7 @@ Created on Mon Apr 13 17:53:57 2020
 
 import REP_CVX as cvx
 kenya = cvx.C_SUT(r'Database\Kenya_2014_SAM.xlsx')
-sh_path = r'Interventions\New folder\cover\0.33.xlsx'
+sh_path = r'Interventions\New folder\banan\20.xlsx'
 kenya.shock(path = sh_path, Y = True )
 
 kenya.calc_all()
@@ -43,7 +43,7 @@ kenya.add_dict()
 #%%
 #results= kenya.results
 #%%
-kenya.Int_Ass(sce_name='0.33')
+kenya.Int_Ass(sce_name='20')
 #print(kenya.ROI)
 #%%
 #am = kenya.X.index.get_level_values(0,1)
