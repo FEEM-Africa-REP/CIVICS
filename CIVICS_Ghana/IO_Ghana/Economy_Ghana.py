d32832f7a01d4ea46d10b5b9f41442e8cc449b03
nigolred <nicolo.golinucci@gmail.com>
2020-11-09 09:58:18 +0100

    Economic analysis

--- CIVICS_Ghana/IO_Ghana/Economy_Ghana.py
+++ CIVICS_Ghana/IO_Ghana/Economy_Ghana.py
@@ -5,7 +5,7 @@ Created on Thu Nov  5 18:03:02 2020
 @author: nigolred
 """
 import sys
-sys.path.insert(1,r'CIVICS')
+sys.path.insert(1,r'C:\Users\Gollinucci\Documents\GitHub\CIVICS')
 
 import REP_CVX
 
@@ -19,13 +19,14 @@ z_15 = gha15.z
 va_15 = gha15.va
 VA_15 = gha15.VA
 Y_15 = gha15.Y
+X_15 = gha15.X
 
 col_sum_15 = z_15.sum() + va_15.sum()
 COL_SUM_15 = Z_15.sum() + VA_15.sum()
 ROW_SUM_15 = Z_15.sum(axis=1) + Y_15.sum(axis=1)
 zero = COL_SUM_15 - ROW_SUM_15
 
-GDP_15 = VA_15.groupby(level=1).sum().groupby(axis=1, level=2).sum().drop(['Import'])
+GDP_15 = VA_15.groupby(level=1).sum().groupby(axis=1, level=2, sort=False).sum()
 GDP_15_tot = GDP_15.sum().sum() / 1000 # Value in B LCU
 sGDP_15 = GDP_15.sum() / GDP_15.sum().sum()
 
@@ -36,13 +37,14 @@ z_13 = gha13.z
 va_13 = gha13.va
 VA_13 = gha13.VA
 Y_13 = gha13.Y
+X_13 = gha13.X
 
 col_sum_13 = z_13.sum() + va_13.sum()
 COL_SUM_13 = Z_13.sum() + VA_13.sum()
 ROW_SUM_13 = Z_13.sum(axis=1) + Y_13.sum(axis=1)
 zero = COL_SUM_13 - ROW_SUM_13
 
-GDP_13 = VA_13.groupby(level=1).sum().groupby(axis=1, level=2).sum().drop(['Import'])
+GDP_13 = VA_13.groupby(level=1).sum().groupby(axis=1, level=2, sort=False).sum()
 GDP_13_tot = GDP_13.sum().sum() / 1000 # Value in B LCU
 sGDP_13 = GDP_15.sum() / GDP_15.sum().sum()
 
