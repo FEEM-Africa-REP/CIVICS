e96cfb1bf979fa3b9a87b047241ba8b2d445eb18
nigolred <nicolo.golinucci@gmail.com>
2020-12-03 17:32:19 +0100

    Updated aggregated sectors

--- CIVICS_Ghana/IO_Ghana/Pumpfed Irrigation for Maize.py
+++ CIVICS_Ghana/IO_Ghana/Pumpfed Irrigation for Maize.py
@@ -14,14 +14,14 @@ Ghana = REP_CVX.C_SUT(path = r'Database\GHANA_2015_CVX.xlsx', unit = 'M GHS', na
 #%%
 Ghana.shock_calc(path=r'Pumpfed Irrigation for Maize/Shock1_inputs.xlsx', Y=True)
 Ghana.plot_dx()
-Ghana.plot_dv(level='Activities', title='Required Value Added for producing the wells')
-Ghana.plot_dv(level='Commodities', title='Required Commodities for producing the wells')
+Ghana.plot_dv(level='Activities', title='Required Value Added for producing the wells and pumps')
+Ghana.plot_dv(level='Commodities', title='Required Commodities for producing the wells and pumps')
 
 #%%
 Ghana.shock_calc(path=r'Pumpfed Irrigation for Maize/Shock1_inputs.xlsx', Z=True)
 
-Ghana.plot_dv(level='Activities', title='Saved Value Added for using the wells')
-Ghana.plot_dv(level='Commodities', title='Saved Commodities for using the wells')
+Ghana.plot_dv(level='Activities', title='Saved Value Added using pumpfed irrigation')
+Ghana.plot_dv(level='Commodities', title='Saved imports for using pumpfed irrigation', drop=['Traders','Indirect Taxes'], color='red')
 
 Ghana.impact_assess(p_life=10, saving_sce=['sh', 2], invest_sce=['sh',1], labour=['Labor'], capital=['Capital'])
 
