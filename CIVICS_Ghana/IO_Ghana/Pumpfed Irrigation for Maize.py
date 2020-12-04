af9dc3632f07825d2fd207afab28331be238ae66
nigolred <nicolo.golinucci@gmail.com>
2020-12-04 08:59:39 +0100

    Minor adjustments on the choice of factors

--- CIVICS_Ghana/IO_Ghana/Pumpfed Irrigation for Maize.py
+++ CIVICS_Ghana/IO_Ghana/Pumpfed Irrigation for Maize.py
@@ -22,7 +22,7 @@ Ghana.shock_calc(path=r'Pumpfed Irrigation for Maize/Shock1_inputs.xlsx', Z=True
 
 Ghana.plot_dv(level='Activities', title='Saved Value Added using pumpfed irrigation')
 Ghana.plot_dv(level='Commodities', title='Saved imports for using pumpfed irrigation', drop=['Traders','Indirect Taxes'], color='red')
-Ghana.plot_ds(indicator='Water', color=['Blue','Green','Gray'])
+Ghana.plot_ds(indicator='CO2')
 
-Ghana.impact_assess(p_life=10, saving_sce=['sh', 2], invest_sce=['sh',1], labour=['Labor'], capital=['Capital'])
+Ghana.impact_assess(p_life=10, saving_sce=['sh', 2], invest_sce=['sh',1])
 
