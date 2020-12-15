07e7fd4770c34b645e0e2133e9890dadca134244
nigolred <nicolo.golinucci@gmail.com>
2020-12-15 11:15:01 +0100

    Results_shocks

--- CIVICS_Ghana/IO_Ghana/Indipendent Power Supply for Feed Mills.py
+++ CIVICS_Ghana/IO_Ghana/Indipendent Power Supply for Feed Mills.py
@@ -11,41 +11,84 @@ sys.path.insert(1,r'C:\Users\Gollinucci\Documents\GitHub\CIVICS')
 import REP_CVX
 
 Case = 'Indipendent Power Supply for Feed Mills'
-Scenario = '50% solar'
+Scenarios = ['50% hybrid','100% hybrid', '50% solar','100% solar']
+Delta = {}
 
-Ghana = REP_CVX.C_SUT(path = r'C:\Users\Gollinucci\Documents\GitHub\CIVICS\CIVICS_Ghana\IO_Ghana\Database\GHANA_2015_CVX.xlsx', unit = 'M GHS', name=Case)
-#%%
-Ghana.shock_calc(path=r'Indipendent Power Supply for Feed Mills/'+Scenario+'.xlsx', Y=True)
-Ghana.plot_dx()
-Ghana.plot_dv(level='Activities', title='Required Value Added for producing the mini-grid for the mills in the Scenario '+Scenario)
-Ghana.plot_dv(level='Commodities', title='Required Commodities for producing the mini-grid for the mills in the Scenario '+Scenario)
+for Scenario in Scenarios:
 
-#%%
-Ghana.shock_calc(path=r'Indipendent Power Supply for Feed Mills/'+Scenario+'.xlsx', Z=True, VA=True)
+    Ghana = REP_CVX.C_SUT(path = r'C:\Users\Gollinucci\Documents\GitHub\CIVICS\CIVICS_Ghana\IO_Ghana\Database\GHANA_2015_CVX.xlsx', unit = 'M GHS', name=Case)
+    #%%
+    
+    Ghana.shock_calc(path=r'Indipendent Power Supply for Feed Mills/'+Scenario+'.xlsx', Y=True)
+    # Ghana.plot_dx()
+    # Ghana.plot_dv(level='Activities', title='Required Value Added for producing the mini-grid for the mills in the Scenario '+Scenario)
+    # Ghana.plot_dv(level='Commodities', title='Required Commodities for producing the mini-grid for the mills in the Scenario '+Scenario)
+    
+    #%% 
+    Ghana.shock_calc(path=r'Indipendent Power Supply for Feed Mills/'+Scenario+'.xlsx', Z=True, VA=True, S=True)
+    
+    # Ghana.plot_dv(level='Activities', title='Saved Value Added using the mini-grid for grain milling in the Scenario '+Scenario)
+    # Ghana.plot_dv(level='Commodities', title='Saved imports for using the mini-grid for grain milling in the Scenario '+Scenario)
+    # Ghana.plot_ds(indicator='Water', color=['Blue','Green','Grey'])
+    # Ghana.plot_ds(indicator='GHG')
+    # Ghana.plot_ds(indicator='CO2', detail=False)
+    # Ghana.plot_ds(indicator='Land')
+    # Ghana.plot_ds(indicator='CH4', detail=True)
+    # Ghana.plot_ds(indicator='N2O', detail=False)
+    
+    
+    Res = Ghana.results
+    
+    
+    #%%
+    # Ghana.sensitivity(r'Indipendent Power Supply for Feed Mills/'+Scenario+'.xlsx')
+    # Ghana.plot_sens('S', sc_num=2, indicator='Water')
+    #%%
+    '''
+    Ghana.impact_assess(p_life=10, saving_sce=['sh', 2], invest_sce=['sh',1],imports=['Import'],
+                        w_ext=['Water'], em_ext=['CO2'], land=['Land'], 
+                        labour=['Labor'],
+                        capital=['Capital'], save_excel=True)
+    # #%%
+    # Ghana.impact_assess(p_life=10, saving_sce=['sh', 2], invest_sce=['se',2],imports=['Import'],
+    #                     w_ext=['Water'], em_ext=['GHG'], land=['Land'], 
+    #                     labour=['Labor'],
+    #                     capital=['Capital'], save_excel=True)
+    '''
+    Delta[Scenario] = Res['shock_2']['S_agg']-Res['baseline']['S_agg']
+    Delta[Scenario] = Delta[Scenario]['Activities'].drop('unused')
+#%% Plot
 
-Ghana.plot_dv(level='Activities', title='Saved Value Added using the mini-grid for grain milling in the Scenario '+Scenario)
-Ghana.plot_dv(level='Commodities', title='Saved imports for using the mini-grid for grain milling in the Scenario '+Scenario)
-Ghana.plot_ds(indicator='Water', color=['Blue','Green','Grey'])
-Ghana.plot_ds(indicator='GHG')
-# Ghana.plot_ds(indicator='CO2', detail=False)
-# Ghana.plot_ds(indicator='CH4', detail=False)
-# Ghana.plot_ds(indicator='N2O', detail=False)
 
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
-Res = Ghana.results
+fig = make_subplots(rows=1, cols=len(Scenarios), subplot_titles=Scenarios, shared_yaxes=True)
+colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000']
+sectors = list(Delta[Scenarios[0]].columns)
+indicators = list(Delta[Scenarios[0]].index)
+ind_unit = ['{} [{}]'.format(i,j) for i,j in indicators]
 
+for c in Scenarios:
+    for s in sectors:
+        if Scenarios.index(c)==0:
+            sl = True
+        else:
+            sl = False
+        fig.add_trace(go.Bar(x=ind_unit,
+                             y=Delta[c][s].values,
+                             name=s,
+                             legendgroup=s,
+                             marker_color=colors[sectors.index(s)],
+                             showlegend=sl), row=1, col=Scenarios.index(c)+1)
+    fig.add_trace(go.Scatter(x=ind_unit,
+                             y=Delta[c].sum(axis=1),
+                             name='Overall',
+                             legendgroup='Total',
+                             marker_color='black',
+                             mode='markers',
+                             showlegend=sl), row=1, col=Scenarios.index(c)+1)
 
-#%%
-# Ghana.sensitivity(r'Indipendent Power Supply for Feed Mills/'+Scenario+'.xlsx')
-# Ghana.plot_sens('S', sc_num=2, indicator='Water')
-#%%
+fig.update_layout(title='Use of Resources by Scenario with respect to the baseline with the dedicated mini-grid', barmode='relative', legend_title_text='Sectors')
+fig.write_html('Resource Use.html')
 
-Ghana.impact_assess(p_life=10, saving_sce=['sh', 2], invest_sce=['sh',1],imports=['Import'],
-                    w_ext=['Water'], em_ext=['GHG'], land=['Land'], 
-                    labour=['Labor'],
-                    capital=['Capital'], save_excel=True)
-# #%%
-# Ghana.impact_assess(p_life=10, saving_sce=['sh', 2], invest_sce=['se',2],imports=['Import'],
-#                     w_ext=['Water'], em_ext=['GHG'], land=['Land'], 
-#                     labour=['Labor'],
-#                     capital=['Capital'], save_excel=True)
--- CIVICS_Ghana/IO_Ghana/Pumpfed Irrigation for Maize.py
+++ CIVICS_Ghana/IO_Ghana/Pumpfed Irrigation for Maize.py
@@ -23,14 +23,8 @@ Ghana.shock_calc(path=r'Pumpfed Irrigation for Maize/Shock1_inputs.xlsx', Z=True
 Ghana.plot_dv(level='Activities', title='Saved Value Added using pumpfed irrigation')
 Ghana.plot_dv(level='Commodities', title='Saved imports for using pumpfed irrigation', drop=['Traders','Indirect Taxes'], color='red')
 Ghana.plot_ds(indicator='Water', color=['Blue','Green','Grey'])
-Ghana.plot_ds(indicator='GHG')
 Ghana.plot_ds(indicator='CO2', detail=False)
-Ghana.plot_ds(indicator='CH4', detail=False)
-Ghana.plot_ds(indicator='N2O', detail=False)
-
-
 Res = Ghana.results
-
 
 #%%
 Ghana.sensitivity(r'Pumpfed Irrigation for Maize/Shock1_inputs.xlsx')
@@ -38,12 +32,12 @@ Ghana.plot_sens('S', sc_num=2, indicator='Water')
 #%%
 
 Ghana.impact_assess(p_life=10, saving_sce=['se', 3], invest_sce=['sh',1],imports=['Import'],
-                    w_ext=['Water'], em_ext=['GHG'], land=['Land'], 
+                    w_ext=['Water'], em_ext=['CO2'], land=['Land'], 
                     labour=['Labor'],
                     capital=['Capital'], save_excel=True)
 #%%
 Ghana.impact_assess(p_life=10, saving_sce=['sh', 2], invest_sce=['se',2],imports=['Import'],
-                    w_ext=['Water'], em_ext=['GHG'], land=['Land'], 
+                    w_ext=['Water'], em_ext=['CO2'], land=['Land'], 
                     labour=['Labor'],
                     capital=['Capital'], save_excel=True)
 
